92b8e10daaba9165d274f5222b7e2191c0422666
mtenorio50 <270647234@yoobeestudent.ac.nz>
2025-09-10 11:58:38 +1200

    part 2 bank app done

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ function updateRoute() {
     } else {
         console.log('God is good');
     }
-
     const template = document.getElementById(route.templateId);
     const view = template.content.cloneNode(true);
     const app = document.getElementById('app');
     app.innerHTML = '';
     app.appendChild(view);
-}
+};
 
 function navigate(path) {
     window.history.pushState({}, path, path);
@@ -39,6 +38,32 @@ function onLinkClick(event) {
     event.preventDefault();
     navigate(event.target.href);
 };
+
+async function register() {
+    const registerForm = document.getElementById('registerForm');
+    const formData = new FormData(registerForm);
+    const data = Object.fromEntries(formData);
+    const jsonData = JSON.stringify(data);
+    const result = await createAccount(jsonData);
+
+    if (result.error) {
+        return console.log('An error occurred:', result.error);
+    }
+    console.log('Account created!', result);
+};
+
+async function createAccount(account) {
+    try {
+        const response = await fetch('//localhost:5000/api/accounts', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: account
+        });
+        return await response.json();
+    } catch (error) {
+        return { error: error.message || 'Unknown error' };
+    }
+}
 
 window.onpopstate = () => updateRoute();
 updateRoute();
